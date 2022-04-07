c802167a48ff53e6719e6e2be3126ef8a0ee7f26
Leo Lee <82523767+leo3266@users.noreply.github.com>
2022-04-07 16:12:23 -0400

    Update Test1.java
    changed to if else

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+  
 enum Operation
 {
     ADD, SUBTRACT, MULTIPLY, DIVIDE
@@ -9,25 +9,22 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
+        if (operation.equals("ADD")) {
+
+            // if operation = ADD, return the sum
+            result = num1 + num2;
+        } else if (operation.equals("SUBTRACT")) {
+            result = num1 - num2;
+        } else if (operation.equals("MULTIPLY")) {
                 result = num1 * num2;
-                break;
-            case DIVIDE:
+        } else if (operation.equals("DIVIDE")) {
                 if (num2 == 0) {
                     System.out.println("Can't divide by 0");
                 } else {
                     result = ((double)num1) / num2;
                 }
-                break;
         }
+
         return result;
     }
 
